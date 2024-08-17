f541fe63992d97b21b0fd831a2481467f5f0bf08
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-17 11:35:30 +0000

    ---

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Lumina-AI",
+  base: "/Lumina",
   plugins: [react()],
   assetsInclude: ["**/*.{JPG,PNG,jpg,png}"],
   optimizeDeps:{

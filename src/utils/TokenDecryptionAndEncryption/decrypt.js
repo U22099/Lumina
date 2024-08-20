4dbe6b088f7c54cc366a40dacfd75b03cd051bcd
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-21 00:46:44 +0100

    Update decrypt.js

--- src/utils/TokenDecryptionAndEncryption/decrypt.js
+++ src/utils/TokenDecryptionAndEncryption/decrypt.js
@@ -1,6 +1,8 @@
-function decrypt(string){
+function decrypt(stringArr){
     let decryptedArr = [];
-    string.forEach( str => {
+    console.log(stringArr);
+    stringArr.forEach( str => {
+        console.log(str);
         const decodedChunk = [];
         for(let x = 0; x < str.length; x+=3){
             const index = parseInt(swap(`${str[x+1]}${str[x+2]}`), 10);

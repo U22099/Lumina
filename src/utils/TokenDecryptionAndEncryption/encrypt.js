9c1b257ebe7c59fb475686e10f338474fcc1d751
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-21 00:23:56 +0100

    Update encrypt.js

--- src/utils/TokenDecryptionAndEncryption/encrypt.js
+++ src/utils/TokenDecryptionAndEncryption/encrypt.js
@@ -7,10 +7,11 @@ function encrypt(token){
     for(let x = 0; x < Math.ceil(tokenArr.length/100); x++){
         chunkArr.push(tokenArr.splice(100));
     }
-    chunkArr.map(chunk => {
+    console.log(chunkArr);
+    chunkArr.forEach(chunk => {
         let arr = [];
         for(let x = 0; x < chunk.length; x++){
-            arr.push(`${token[x]}${swap(x)}`);
+            arr.push(`${chunk[x]}${swap(x)}`);
         }
         encryptedChunkArr.push(shuffle(arr).join(''));
     });

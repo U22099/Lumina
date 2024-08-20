c772330efc4c037304e97b34ece3a3355b704a82
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-20 10:57:12 +0100

    Add files via upload

--- src/utils/TokenDecryptionAndEncryption/decrypt.js
+++ src/utils/TokenDecryptionAndEncryption/decrypt.js
@@ -1,6 +1,6 @@
 function decrypt(str){
     const decryptedArr = new Array(Math.ceil(str.length / 3));
-    for(let x = 0; x < arr.length; x+=3){
+    for(let x = 0; x < str.length; x+=3){
         const index = parseInt(swap(`${str[x+1]}${str[x+2]}`), 10);
         decryptedArr[index] = str[x];
     }

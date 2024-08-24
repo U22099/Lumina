d85310036191a0189c15cb8bde369f6a7bf2e61b
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-24 08:36:29 +0100

    Add files via upload

--- src/utils/indexedDB.js
+++ src/utils/indexedDB.js
@@ -2,7 +2,6 @@ const init = () => {
     const opendb = indexedDB.open("Lumina");
     opendb.onupgradeneeded = (event) => {
         const db = event.target.result;
-        db.createObjectStore("AiImage");
         db.createObjectStore("ChatData");
         db.createObjectStore("UserData");
     }

950e0fe291525ff1f39843d9462123d7d0b962ea
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-25 18:23:26 +0100

    Add files via upload

--- src/utils/getAiImage.js
+++ src/utils/getAiImage.js
@@ -1,7 +1,7 @@
 import storage from './localStorage.js';
 import indexedDB from './indexedDB.js';
 import toBase64 from './base64';
-import logo from 'logo.jpg';
+import logo from '../public/logo.jpg';
 
 const getAiImage = async (setImage) => {
     const data = await indexedDB.getData("AI_Image");

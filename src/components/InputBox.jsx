c49192e18be1544fbde524e1e5a39055d827943c
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-25 09:43:40 +0100

    Add files via upload

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -9,11 +9,11 @@ const InputBox = () => {
               e.target.style.height = `${e.target.scrollHeight}px`;
   }
   return (
-    <div className="bg-gray-100 rounded-full border-0 ring-1 ring-inset ring-transparent  focus:ring-inset focus:ring-indigo-600 focus:ring-3 focus-within:ring-inset focus-within:ring-[var(--secondary-color)] dark:bg-[var(--accent-color)] w-[90%] py-2 px-4 flex fixed left-[5%] bottom-6 md:bottom-10 h-fit">
+    <div className="bg-gray-100 rounded-full border-0 ring-1 ring-inset ring-transparent  focus:ring-inset focus:ring-indigo-600 focus:ring-3 focus-within:ring-inset focus-within:ring-[var(--secondary-color)] dark:bg-[var(--accent-color)] w-[90%] py-2 px-4 flex fixed left-[5%] bottom-6 md:bottom-10 h-fit items-center">
       <textarea
         rows="1"
         type="text"
-        className="resize-none bg-none bg-transparent outline-none w-full placeholder:font-semibold comic-neue-bold text-black dark:text-white mx-7 my-auto h-5 max-h-20"
+        className="resize-none bg-none bg-transparent outline-none w-full placeholder:font-semibold comic-neue-bold text-black dark:text-white mx-7 h-5 max-h-20"
         onKeyPress={autoResize}
 		  onKeyUp={autoResize}
         autoComplete="off"
@@ -38,7 +38,7 @@ const InputBox = () => {
 
         <button
           tabIndex={0}
-          className="bg-[var(--secondary-color)] px-8 py-2 rounded-[2rem] outline-0"
+          className="bg-[var(--secondary-color)] px-6 md:px-8 py-2 rounded-[2rem] outline-0"
         >
           <IoMdSend className="w-5 h-5" />
         </button>

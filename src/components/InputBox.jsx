bb84aa6950369f0aa9214c04a0977d7932337aa0
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-25 08:51:22 +0100

    Add files via upload

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -4,21 +4,18 @@ const InputBox = () => {
   const handleFileChange = (e) => {
     console.log(e.target.files[0].name);
   };
-
+  const autoResize = (e) => {
+	   e.target.style.height = "56px";
+              e.target.style.height = `${e.target.scrollHeight}px`;
+  }
   return (
-    <div className="bg-gray-100 rounded-full border-0 ring-1 ring-inset ring-transparent  focus:ring-inset focus:ring-indigo-600 focus:ring-3 focus-within:ring-inset focus-within:ring-[var(--secondary-color)] dark:bg-[var(--accent-color)] w-[90%] py-2 px-4 flex fixed left-[5%] bottom-6 md:bottom-10 h-16">
+    <div className="bg-gray-100 rounded-full border-0 ring-1 ring-inset ring-transparent  focus:ring-inset focus:ring-indigo-600 focus:ring-3 focus-within:ring-inset focus-within:ring-[var(--secondary-color)] dark:bg-[var(--accent-color)] w-[90%] py-2 px-4 flex fixed left-[5%] bottom-6 md:bottom-10 h-fit">
       <textarea
-        rows={1}
+        rows={0}
         type="text"
         className="resize-none bg-none bg-transparent outline-none w-full placeholder:font-semibold comic-neue-bold text-black dark:text-white mx-8 my-auto h-14 max-h-20"
-        onKeyPress={
-          (e) => {
-            if(e.key === 'Enter'){
-              e.target.style.height = "56px";
-              e.target.style.height = `${e.target.scrollHeight}px`;
-            }
-          }
-        }
+        onKeyPress={autoResize}
+		  onKeyUp={autoResize}
         autoComplete="off"
         placeholder="Message Lumina"
         tabIndex={0}

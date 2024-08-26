c522200592393fdbc88bcc33f69ce59c7c8916d6
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-26 03:57:15 +0100

    Add files via upload

--- src/store.js
+++ src/store.js
@@ -1,20 +1,7 @@
 import { create } from 'zustand';
 
 const useChat = create((set) => ({
-    chat: [
-        {
-            role: 'user',
-            parts: [{
-                text: 'Your name is Lumina, a text-based Ai created by Daniel using gemini api. Make sure all your response are formatted in html'
-            }]
-        },
-        {
-            role: 'model',
-            parts: [{
-                text: 'Understood! so how may i help you today'
-            }]
-        }
-    ],
+    chat: [],
     setChat: (data) => {
         set({chat: data});
     }

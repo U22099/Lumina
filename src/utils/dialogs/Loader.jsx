1fbcfe4acd05dd9640f01a187978a7d8ede0b422
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-26 19:44:36 +0100

    Add files via upload

--- src/utils/dialogs/Loader.jsx
+++ src/utils/dialogs/Loader.jsx
@@ -12,7 +12,7 @@ function Loader() {
                     duration: 0.5,
                 }
             }}
-            className="flex w-[100vw] h-[100vh] justify-center items-center backdrop-blur-sm">
+            className="fixed flex w-[100vw] h-[100vh] justify-center items-center backdrop-blur-sm">
             <div id="load" className="w-14 h-14 flex items-center">
             <div></div>
             <div></div>

2b95f00fe2013ad6be53b57dd89fd948c003b259
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-29 00:19:44 +0100

    ---

--- src/components/Speaker.jsx
+++ src/components/Speaker.jsx
@@ -12,6 +12,7 @@ const Speaker = ({ setSpeaking, text, setStart }) => {
   const speech = window.speechSynthesis || speechSynthesis;
   const utterance = new SpeechSynthesisUtterance();
   useEffect(() => {
+    console.log(text);
     utterance.text = text;
     const voices = speech.getVoices();
     if (voices.length > 0) {
@@ -21,15 +22,19 @@ const Speaker = ({ setSpeaking, text, setStart }) => {
       setSpeaking(false);
       setStart(false);
     };
-    utterance.onerror = () => {
-      setSpeaking(false);
-      setStart(false);
+    utterance.onerror = (error) => {
+        console.log(error)
+        setReturnedText("An error occured");
+        setTimeout(() => {
+          setSpeaking(false);
+          setStart(false);
+        }, 2000);
     };
     speech.speak(utterance);
   }, [text]);
   return (
     <>
-        <div className="flex items-center justify-center overflow-hidden overflow-y-scroll scrollbar bg-gray-100 shadow-md rounded-md w-fit max-w-[80vw] h-fit max-h-[50vh] md:max-w-[60vw] break-words whitespace-wrap p-3 mx-auto">
+        <div className="flex flex-col items-center justify-center overflow-hidden overflow-y-scroll scrollbar bg-gray-100 shadow-md rounded-md w-fit max-w-[80vw] h-fit max-h-[50vh] md:max-w-[60vw] break-words whitespace-wrap p-3 mx-auto">
         <p className="comic-neue-bold text-black dark:text-white">
             {returnedText}
         </p>

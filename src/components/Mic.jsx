1e79586b44e4651c8966ce76ae8ac4fbce60fd8a
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-28 03:45:43 +0100

    Add files via upload

--- src/components/Mic.jsx
+++ src/components/Mic.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { FaMicrophoneLines, FaMicrophoneLinesSlash } from 'react-icons/fa6';
 

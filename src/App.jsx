25a9ebe66c0857f06639bc75541702d64dbb921a
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-29 09:26:36 +0000

    —-

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
   const [processing, setProcessing] = useState(false);
   const [initSpeech, setInitSpeech] = useState(false);
   return (
-    <div className={(voiceInput ? "grid-rows-[2fr_8fr_3fr] " : "grid-rows-[1fr_10fr_1.5fr] ") +"h-dvh md:h-dvh w-screen dark:bg-[var(--primary-color)] bg-white mx-auto relative overflow-hidden grid"}>
+    <div className={(voiceInput ? "grid-rows-[2fr_8fr_3fr] " : "grid-rows-[1fr_10fr_1.5fr] ") +"h-svh md:h-dvh w-screen dark:bg-[var(--primary-color)] bg-white mx-auto relative overflow-hidden grid"}>
       <Header userName={userName} userImage={userImage} setUserName={setUserName} setUserImage={setUserImage} setVoiceInput={setVoiceInput} voiceInput={voiceInput}/>
       {voiceInput ? <Text speaking={speaking} listening={listening} processing={processing}/> :<Message userImage={userImage} loading={loading}/>}
       {voiceInput ? <Speech setListening={setListening} setSpeaking={setSpeaking} setProcessing={setProcessing} speaking={speaking} initSpeech={initSpeech} setInitSpeech={setInitSpeech}/> :<InputBox loading={loading} setLoading={setLoading}/>}

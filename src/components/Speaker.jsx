eb600abf25b3d752bd53e8cc1023c2f6a93f90aa
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-29 01:21:58 +0100

    ---

--- src/components/Speaker.jsx
+++ src/components/Speaker.jsx
@@ -37,21 +37,21 @@ const Speaker = ({ setSpeaking, text, setStart }) => {
   }, [text]);
   return (
     <div className="flex flex-col items-center justify-center max-w-[80vw] md:max-w-[60vw] mx-auto gap-2">
-        <div className="flex items-center justify-center overflow-hidden overflow-y-scroll scrollbar bg-gray-100 shadow-md rounded-md w-fit max-w-[80vw] h-fit max-h-[50vh] md:max-w-[60vw] break-words whitespace-wrap p-3 mx-auto">
+        <div className="flex items-center justify-center overflow-hidden overflow-y-scroll scrollbar bg-gray-100 shadow-md rounded-md w-fit max-w-[80vw] h-fit max-h-[50vh] md:max-w-[60vw] break-words whitespace-wrap p-3 mx-auto mt-8">
         <p className="comic-neue-bold text-black dark:text-white">
             {returnedText}
         </p>
         </div>
-        <p className="comic-neue-bold text-black dark:text-white w-[80vw] md:w-[60vw] h-fit py-3 bg-[var(--secondary-color)] rounded-md shadow-md flex justify-center items-center" onClick={() => {speech.speak(utterance)}}>Replay</p>
+        <p className="comic-neue-bold text-black dark:text-white w-[80vw] md:w-[60vw] h-fit py-3 bg-[var(--secondary-color)] rounded-md shadow-md flex justify-center items-center mb-4" onClick={() => {speech.speak(utterance)}}>Replay</p>
         <p className="comic-neue-bold text-black dark:text-white w-[80vw] md:w-[60vw] h-fit py-3 bg-[var(--secondary-color)] rounded-md shadow-md flex justify-center items-center" onClick={() => {speech.cancel(); setSpeaking(false); setStart(false)}}>Skip</p>
     </div>
   );
 };
 
 function getFemaleVoice(voices) {
-  const femaleVoices = voices.filter(
-    voice => voice.gender === "female"
+  const preferedVoice = voices.filter(
+    voice => (voice.gender === "female"&&voice.lang === "en-GB") || voice.lang === "en-GB"
   );
-  return femaleVoices[0] || voices[0];
+  return preferedVoice[0];
 }
 export default Speaker;

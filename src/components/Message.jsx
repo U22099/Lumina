ecf81bbe55029049b190442f33b14471ae8a691f
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-29 13:29:32 +0000

    —-

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -63,7 +63,10 @@ const ChatUser = ({x, userImage}) => {
                 {x.parts.map((part) => {
                     return ( 
                         <div>
-                            {part.inlineData.mimeType.includes("image")&&<img src={`data:${part.inlineData.mimeType};base64,${part.inlineData.data}`} alt="Lumina" className="rounded-md w-40 h-40 mx-auto object-cover"/>}
+                            {part.inlineData.mimeType.includes("image")&&<img src={`data:${part.inlineData.mimeType};base64,${part.inlineData.data}`} alt="Image" className="rounded-md w-40 h-40 mx-auto object-cover"/>}
+                            {part.inlineData.mimeType.includes("audio")&&<audio src={`data:${part.inlineData.mimeType};base64,${part.inlineData.data}`} alt="Audio" className="rounded-md w-full h-40 mx-auto " controls/>}
+                            {part.inlineData.mimeType.includes("video")&&<video src={`data:${part.inlineData.mimeType};base64,${part.inlineData.data}`} alt="Video" className="rounded-md w-40 h-40 mx-auto object-cover" controls/>}
+                            {part.inlineData.mimeType.includes("application")&&<embed src={`data:${part.inlineData.mimeType};base64,${part.inlineData.data}`} alt="PDF" className="rounded-md w-40 h-40 mx-auto object-cover"/>}
                             <div className="comic-neue-bold text-black dark:text-white display text-wrap max-w-[70vw] md:max-w-[50vw] w-fit break-words whitespace-pre-wrap overflow-hidden" dangerouslySetInnerHTML={{__html: part.text}}/> 
                         </div>
                     )

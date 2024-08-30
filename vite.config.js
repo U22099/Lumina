3737d0893151115ed3ce4e4d751cd8596c62424b
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-30 13:31:26 +0000

    Configuring PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// https://vitejs.dev/config/
+import {VitePWA} from 'vite-plugin-pwa';
+  // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Lumina",
-  plugins: [react()],
+  plugins: [react(),
+  VitePWA({
+      manifest: {
+        name: 'Lumina',
+        short_name: 'lumina',
+        description: 'Lumina Ai chatbot',
+        theme_color: '#1c1c22',
+        icons: [ 
+	{ src: "pwa-64x64.png", sizes: "64x64", type: "image/png" }, 
+	{ src: "pwa-192x192.png", sizes: "192x192", type: "image/png" } 
+	{ src: "pwa-512x512.png", sizes: "512x512", type: "image/png" }, 
+	{ src: "maskable-icon-512x512.png", sizes: "512x512", type: "image/png", purpose: "maskable" }
+	] 
+      }
+    })
+    ],
   assetsInclude: ["**/*.{JPG,PNG,jpg,png}"],
   optimizeDeps:{
     override: true

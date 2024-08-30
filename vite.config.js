0ac1ef5e274adffe73fe293111d76647561eaa94
Daniel <156960927+U22099@users.noreply.github.com>
2024-08-30 13:36:18 +0000

    —-

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
         theme_color: '#1c1c22',
         icons: [ 
 	{ src: "pwa-64x64.png", sizes: "64x64", type: "image/png" }, 
-	{ src: "pwa-192x192.png", sizes: "192x192", type: "image/png" } 
+	{ src: "pwa-192x192.png", sizes: "192x192", type: "image/png" },
 	{ src: "pwa-512x512.png", sizes: "512x512", type: "image/png" }, 
 	{ src: "maskable-icon-512x512.png", sizes: "512x512", type: "image/png", purpose: "maskable" }
 	] 

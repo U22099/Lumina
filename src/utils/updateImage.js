6bc6d84b229a0f06a03134ba7476245b57eeb1da
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-01 02:18:59 +0000

    Revert "---"
    This reverts commit 35aa4e853fab7975d2314ae1ed5d3eede27048b2.

--- src/utils/updateImage.js
+++ src/utils/updateImage.js
@@ -1,27 +1,27 @@
-import axios from 'axios';
-import refresh from './refresh.js';
-import {getToken} from './token.js';
-import origin from '../../config/origin.json';
+import axios from "axios";
+import refresh from "./refresh.js";
+import { getToken } from "./token.js";
+import origin from "../../config/origin.json";
 import indexedDB from "./indexedDB";
 
 const updateImage = async (setLoad, image, navigate) => {
   setLoad(true);
-    try {
-      const url = `${origin.default.origin}/user?token=${getToken('__A')}`&_id=${getToken('_ID');
-
-    = await patcimage}, {withCredentials: true});
-      stLoad(fals    cost data = await indexedDBta("UserData");
-      indexedDB.saveData([...data, image], "UserData");
-    } catch (err) {
-      if ([401, 403].includes(err.response.status)) {
-        const res = await refresh(navigate);
-        if (res.status === 200) {
-          updateImage(setLoad, image, navigate);
-        }
-      } else {
-        console.log(err);
+  try {
+    const url = `${origin.default.origin}/user?token=${getToken("__A")}&_id=${getToken("_ID")}`;
+    await axios.patch(url, { image }, { withCredentials: true });
+    setLoad(false);
+    const data = await indexedDB.getData("UserData");
+ indexedDB.saveData([...data, image], "UserData");
+  } catch (err) {
+    if ([401, 403].includes(err.response.status)) {
+      const res = await refresh(navigate);
+      if (res.status === 200) {
+        updateImage(setLoad, image, navigate);
       }
+    } else {
+      console.log(err);
     }
-  };
+  }
+};
 
-  export default updateImage
+export default updateImage;

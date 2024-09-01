5b709017212b8b0ee57b2ddd3e88ec2ccf22b1fb
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-01 13:46:50 +0000

    —-

--- src/utils/dialogs/ConfirmDialog.jsx
+++ src/utils/dialogs/ConfirmDialog.jsx
@@ -26,15 +26,15 @@ function ConfirmDialog({var2, callback, msg, setLoad}) {
                 }}
                 className="flex flex-col bg-gray-100 dark:bg-[var(--accent-color)] rounded-md shadow-md p-8  md:w-[40vw] w-[80vw] justify-center items-center gap-2">
                 <div className="w-[100%] flex justify-center items-center">
-                    <GiConfirmed className="text-red-600 text-bold text-[5em]" />
+                    <GiConfirmed className="fill-[var(--secondary-color)] text-bold text-[5em]" />
                 </div>
                 <h1 className="comic-neue-bold text-[2em] md:text-[2.5em] display flex w-[100%] justify-center items-center dark:text-white text-black">Confirm</h1>
                 <div className=" w-[80%] text-center items-center text-[1.3em]">
                     <p className="comic-neue-regular dark:text-white text-black">{msg}</p>
                 </div>
                 <div className="flex w-[100%] mx-auto gap-[10px]">
-                    <button className="comic-neue-bold w-[50%] bg-red-600 rounded-md text-[2em] shadow-[2px_2px_5px_3px_rgba(0,0,0,0.5)]" onClick={() => ans(true)}>Delete</button>
-                    <button className="comic-neue-bold w-[50%] bg-green-600 rounded-md text-[2em] shadow-[2px_2px_5px_3px_rgba(0,0,0,0.5)]" onClick={() => ans(false)}>Cancel</button>
+                    <button className="comic-neue-bold w-[50%] bg-[var(--secondary-color)] rounded-md text-[2em] shadow-[2px_2px_5px_3px_rgba(0,0,0,0.5)] text-white dark:black" onClick={() => ans(true)}>Delete</button>
+                    <button className="comic-neue-bold w-[50%] bg-black dark:bg-white text-white dark:black rounded-md text-[2em] shadow-[2px_2px_5px_3px_rgba(0,0,0,0.5)]" onClick={() => ans(false)}>Cancel</button>
                 </div>
 
             </motion.div>

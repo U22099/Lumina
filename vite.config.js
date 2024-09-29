7258b175177d00067663f69842c5912ecf7b7c71
Daniel <u22099dandev@gmail.com>
2024-09-29 06:10:57 +0000

    —-

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,9 @@ export default defineConfig({
   VitePWA({
       registerType: 'autoUpdate',
       manifest: {
-        name: 'Lumina AI',
-        short_name: 'Lumina',
-        description: 'Lumina Ai chatbot',
+        name: 'Lumina AI Chatbot',
+        short_name: 'Lumina AI',
+        description: 'Lumina AI is a cutting-edge, multi-modal AI chatbot powered by Gemini API and designed by Swag. Programmed by full-stack web developer Daniel, Lumina combines advanced natural language processing (NLP) with creative and compassionate communication to provide insightful and engaging conversations. Whether you need help with coding, design, brainstorming, or just a friendly chat, Lumina is here to assist you',
         background_color: '#1c1c22', 
         display: 'standalone', 
         start_url: '/Lumina',
@@ -19,11 +19,28 @@ export default defineConfig({
         theme_color: '#1c1c22',
         permissions: ['microphone', 'speechSynthesis'],
         icons: [ 
-	{ src: "pwa-64x64.png", sizes: "64x64", type: "image/png" }, 
-	{ src: "pwa-192x192.png", sizes: "192x192", type: "image/png" },
-	{ src: "pwa-512x512.png", sizes: "512x512", type: "image/png" }, 
-	{ src: "maskable-icon-512x512.png", sizes: "512x512", type: "image/png", purpose: "maskable" }
-	] 
+			{ 
+				src: "pwa-64x64.png", 
+				sizes: "64x64", 
+				type: "image/png" 
+			}, 
+			{ 
+				src: "pwa-192x192.png", 
+				sizes: "192x192", 
+				type: "image/png" 
+			},
+			{ 
+				src: "pwa-512x512.png", 
+				sizes: "512x512", 
+				type: "image/png" 
+			}, 
+			{ 
+				src: "maskable-icon-512x512.png", 
+				sizes: "512x512", 
+				type: "image/png", 
+				purpose: "maskable"
+			}
+		] 
       }
     })
     ],
